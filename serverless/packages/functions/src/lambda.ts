05c826719c70350a2c43fe762b0613dc0e786472
Junior Love <K4ngar0o@proton.me>
2023-05-03 17:01:04 -0500

    Redoing serverless

--- serverless/packages/functions/src/lambda.ts
+++
@@ -1,9 +0,0 @@
-import { ApiHandler } from "sst/node/api";
-import { Time } from "@serverless/core/time";
-
-export const handler = ApiHandler(async (_evt) => {
-  return {
-    statusCode: 200,
-    body: `Hi there, it's Junior - Your boy - ${Time.now()}`,
-  };
-});
